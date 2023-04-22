db4fa632141ce57509e81d6b992371819988b66c
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2023-04-22 17:20:57 +0200

    i really don't like darwin

--- main.go
+++ main.go
@@ -1,75 +1,37 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
-	"net"
 	"os"
 	"syscall"
-
-	"github.com/davecgh/go-spew/spew"
 )
-
-const (
-	BufSize = 4096
-	PortA   = 12345
-	PortB   = 12346
-	PortIn  = 12347
-)
-
-func isWhitelisted(ipAddr uint32) bool {
-	whitelistedIP := binary.BigEndian.Uint32(net.ParseIP("192.168.1.100").To4())
-	return ipAddr == whitelistedIP
-}
 
 func main() {
-	rawSocket, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_IP)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to create raw socket: %v\n", err)
-		os.Exit(1)
+	s := Server{}
+	if err := s.Run(); err != nil {
+		panic(err)
 	}
-	defer syscall.Close(rawSocket)
+}
 
-	err = syscall.SetsockoptInt(rawSocket, syscall.IPPROTO_IP, syscall.IP_HDRINCL, 1)
+type Server struct {
+	buf []byte
+}
+
+func (s *Server) Run() error {
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, syscall.IPPROTO_TCP)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to set socket option: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("failed to create socket: %w", err)
 	}
 
-	destAddr := &syscall.SockaddrInet4{Addr: [4]byte{127, 0, 0, 1}}
-	buffer := make([]byte, BufSize)
+	f := os.NewFile(uintptr(fd), fmt.Sprintf("fd.%d", fd))
 
+	s.buf = make([]byte, 4096)
 	for {
-		n, srcAddr, err := syscall.Recvfrom(rawSocket, buffer, 0)
+		numRead, err := f.Read(s.buf)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to receive packet: %v\n", err)
-			continue
+			fmt.Println(err)
 		}
-
-		bytes := buffer[:n]
-		spew.Dump(bytes)
-
-		ipHeader := bytes[:20]
-		ihl := int(ipHeader[0]&0x0f) << 2
-		transportHeader := bytes[ihl : ihl+8]
-		dstPort := binary.BigEndian.Uint16(transportHeader[2:4])
-
-		if dstPort != PortIn {
-			continue
-		}
-
-		srcAddrInet, _ := srcAddr.(*syscall.SockaddrInet4)
-		srcIP := binary.BigEndian.Uint32(srcAddrInet.Addr[:])
-		if isWhitelisted(srcIP) {
-			destAddr.Port = PortA
-		} else {
-			destAddr.Port = PortB
-		}
-
-		err = syscall.Sendto(rawSocket, ipHeader, 0, destAddr)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to send packet: %v\n", err)
-			continue
-		}
+		bytes := s.buf[:numRead]
+		fmt.Printf("% X\n", bytes)
 	}
 }
